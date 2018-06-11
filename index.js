c7e53e2edfb056d98c4bf697d35b4d3ae3609da6
MisterBOTOn <39442147+MisterBOTOn@users.noreply.github.com>
2018-06-11 19:51:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,75 @@
-const botconfig = require("./botconfig.json");
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "*"
 
-const bot = new Discord.Client({disableEveryone: true})
+client.on('ready', () => {
+  client.user.setGame('*help ', 'https://www.twitch.tv/zzstefanro321/') 
+})   
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setGame("*help Si sunt sclav-ul lui zZstefan");
+   client.on('message', message =>{
+  let args = message.content.split(" ").slice(1);
+
+  if ( message.content.startsWith(prefix+ "say")) {
+    message.delete()
+    const embed = new Discord.RichEmbed()
+    .setDescription(args.join(" "))
+    .setColor(0xff0000)
+   message.channel.sendEmbed(embed);
+  }
+
 });
 
-bot.login(botconfig.token);
+  client.on('message', message => {    
+    if(message.content.startsWith('*mass')) {
+    if(message.author.id === "320887181516210177" ||
+message.author.id === "320887181516210177"){
+       let args = message.content.split(" ").slice(1);         
+       var argresult = args.join(" ")      
+const argsresult = args.join(" ")         
+       let reason = args.join(" ")                   
+    if(!args[1]) {  }  
+    if(args[1]) { 
+message.channel.guild.members.forEach(member => {{ 
+member.send(reason)
+message.delete() }})}}} });
+
+client.on('message', msg => {
+    if (msg.content === '*invite') {
+      msg.channel.send('Invite **** https://discordapp.com/api/oauth2/authorize?client_id=434766536826290177&permissions=8&scope=bot  :tada:');
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === '*support') {
+      msg.channel.send('https://discord.gg/rB2822H');
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === '*info') {
+      msg.channel.send('To set welcome to this server create a channel `joiner`'); 
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === '*help') {
+      msg.channel.send('`? Commands ?` , *invite , *support , *info');
+    }
+  });
+
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'joiner');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('#FF000')
+        .setThumbnail(memberavatar)
+        .addField('✘ | Name: ', `${member}`)
+        .addField('✘ | Welcome !', `Welcome ${member}`)
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+
+client.login(process.env.BOT_TOKEN)
