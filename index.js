0e01946279c1ee74afb38db17ed85382999bfb32
MisterBOTOn <39442147+MisterBOTOn@users.noreply.github.com>
2018-07-05 18:24:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,160 +3,12 @@ const client = new Discord.Client();
 const prefix = "m?"
 
 client.on('ready', () => {
-  client.user.setGame('m?help ', 'https://www.twitch.tv/zzstefanro321')
 })   
 
-  client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-
-  if ( message.content.startsWith(prefix+ "say")) {
-    message.delete()
-    const embed = new Discord.RichEmbed()
-    .setDescription(args.join(" "))
-    .setColor('RANDOM')
-   message.channel.sendEmbed(embed);
-  }
-
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-
-  if ( message.content.startsWith(prefix+ "help")) {
-    const embed = new Discord.RichEmbed()
-    .setTitle('BOT INFO')
-    .setDescription(`•prefix --> m?
-BOT COMMANDS
-• || m?mass || m?support || m?info || m?say || m?ping || m?serverinfo || m?restart || m?avatar || m?purge || m?invite || m?8ball || m?botinfo || `)
-    .setColor('RANDOM')
-   message.channel.sendEmbed(embed);
-  }
-
-});
-
-client.on('message', message => {
-    if (message.author.id == '419472407816830986') {
-    if (message.content.startsWith(`m?restart`)) {
-            resetBot(message.channel);
-    }
-  }
-});
-
-// Turn bot off (destroy), then turn it back on
-function resetBot(channel) {
-    // send channel a message that you're resetting bot [optional]
-    channel.send('Restarting...')
-    .then(msg => bot.destroy())
-    .then(() => bot.login(process.env.TOKEN));
-}
-
- 
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-  
-if ( message.content.startsWith(prefix+ "ping")) {
-    let embed = new Discord.RichEmbed()
-            .setTitle('Ping')
-            .setDescription(`\`\`\`md\nPing :${client.ping}ms\`\`\``)
-            .setColor('RANDOM')
-            .setFooter('Bot pinged')
-            .setTimestamp()
-        
-        message.channel.send(embed);
-  }
-  
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-  
-if (message.content.startsWith(prefix + 'serverinfo')) {
-  const embed = new Discord.RichEmbed()
-  embed.addField('Members', message.guild.memberCount, true)
-  embed.addField('Name', message.guild.name, true)
-  embed.addField('Region', message.guild.region, true)
-  embed.addField('Owner', message.guild.owner, true)
-  embed.addField('ID', message.guild.id, true)
-  embed.setColor('RANDOM')
-  embed.setThumbnail(message.guild.iconURL)
-  message.channel.sendEmbed(embed)
-}
-  
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-  
-if (message.content.startsWith(prefix + 'botinfo')) {
-  const embed = new Discord.RichEmbed()
-  embed.addField('BOT INFO')
-  embed.addField('Name', 'MisterBOT', true)
-  embed.addField('Prefix', 'm?', true)
-  embed.addField('Tag:', '6777', true)
-  embed.addField('Created at:', '2018-06-13 12:52:59.660000', true)
-  embed.addField('ID', '456469970600198155', true)
-  embed.addField('Made with:', 'Node')
-  embed.addField('Creator', '<@320887181516210177>', true)
-  embed.setColor('RANDOM')
-  embed.setThumbnail(message.guild.iconURL)
-  message.channel.sendEmbed(embed)
-}
-  
-});
-
-client.on('message', message =>{
-  let args = message.content.split(" ").slice(1);
-
-  if ( message.content.startsWith("https://discord.gg")) {
-    message.delete()
-    let embed = new Discord.RichEmbed()
-            .setTitle('No Invite Link')
-            .setColor('RANDOM')
-            .setFooter('No invite link!')
-            .setTimestamp()
-        
-        message.channel.send(embed);
-  }
-
-});  
-   
-  client.on('message', message => {    
-    if(message.content.startsWith('m?mass')) {
-    if(message.author.id === "320887181516210177" ||
-message.author.id === "320887181516210177"){
-       let args = message.content.split(" ").slice(1);         
-       var argresult = args.join(" ")      
-const argsresult = args.join(" ")         
-       let reason = args.join(" ")                   
-    if(!args[1]) {  }  
-    if(args[1]) { 
-message.channel.guild.members.forEach(member => {{ 
-member.send(reason)
-message.delete() }})}}} });
-
-client.on('message', msg => {
-    if (msg.content === 'm?invite') {
-      msg.channel.send('Invite **MisterBOT** https://discordapp.com/api/oauth2/authorize?client_id=434766536826290177&permissions=8&scope=bot  :tada:');
-    }
-  });
-
-client.on('message', msg => {
-    if (msg.content === 'm?support') {
-      msg.channel.send('support server ---> https://discord.gg/qaTEp56');
-    }
-  });
-
-client.on('message', msg => {
-    if (msg.content === 'm?info') {
-      msg.channel.send('creatorul meu este : @320887181516210177> ');
-    }
-  });
-
 client.on('guildMemberAdd', member => {
-    var joinrole = member.guild.roles.find('name', '[💪] 𝓣𝓪𝓶𝓮𝓭');
+    var joinrole = member.guild.roles.find('name', '✌ || 𝓜𝒆𝓶𝓫𝓻𝓾 𝓜𝔂𝓽𝓱𝓲𝓬');
     member.addRole(joinrole);
-    let channel  = member.guild.channels.find('name', '😏welcome-bye😥v2');
+    let channel  = member.guild.channels.find('name', '😏welcome-bye😥');
     let memberavatar  = member.user.avatarURL
         if (!channel) return;
         let embed = new Discord.RichEmbed()
@@ -170,7 +22,7 @@ client.on('guildMemberAdd', member => {
 });
 
 client.on('guildMemberRemove', member => {
-    let channel = member.guild.channels.find('name', '😏welcome-bye😥v2');
+    let channel = member.guild.channels.find('name', '😏welcome-bye😥');
     let memberavatar = member.user.avatarURL
         if (!channel) return;
         let embed = new Discord.RichEmbed()
